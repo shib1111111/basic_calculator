56057f3a3aa169c84aec692562985da9d9442d84
Shibkumar <96659370+shib1111111@users.noreply.github.com>
2023-04-09 11:50:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -13,19 +14,45 @@ def app():
     st.title("ANOVA Analysis Tool")
 
     # Add a file uploader so the user can upload a CSV or Excel file
-    data_file = st.file_uploader("Upload a CSV or Excel file", type=["csv", "xlsx"])
+    st.write("Upload your data:")
+    data_file = st.file_uploader("Select a CSV or Excel file", type=["csv", "xlsx"])
+    st.markdown("Or")
+    st.write("Enter your data manually:")
 
-    # If a file has been uploaded, display the data and perform ANOVA analysis
+    num_rows = st.number_input("Number of rows", value=5, min_value=1, max_value=100)
+    num_cols = st.number_input("Number of columns", value=5, min_value=1, max_value=100)
+
+    st.info("Please upload a CSV or Excel file or enter your data manually in the text boxes provided.")
+
+    # Create empty dataframe
+    df = pd.DataFrame()
+
+    # If the user has uploaded a file, use the file data to create the dataframe
     if data_file:
-        df = pd.read_csv(data_file) if data_file.name.endswith(".csv") else pd.read_excel(data_file)
+        try:
+            if data_file.name.endswith(".csv"):
+                df = pd.read_csv(data_file)
+            else:
+                df = pd.read_excel(data_file)
+        except:
+            st.error("Error: Invalid file format or file contents")
+            return
 
-        # Display the data as a table
-        st.write("Data:")
-        st.dataframe(df)
+    # If the dataframe is still empty, get the data from the user input
+    if df.empty:
+        data = [[st.number_input(f"Row {i+1}, Column {j+1}", value=0.0) for j in range(num_cols)] for i in range(num_rows)]
+        df = pd.DataFrame(data, columns=[f"Column {j+1}" for j in range(num_cols)])
 
+    # Display the data as a table
+    st.write("Data:")
+    st.dataframe(df)
+
+    # Add a button to perform the ANOVA analysis
+    if st.button("Perform ANOVA Analysis"):
         # Allow the user to select the dependent and independent variables
         dep_variable = st.selectbox("Select dependent variable", df.columns)
         ind_variable = st.multiselect("Select independent variables", df.columns, default=[df.columns[0]])
+        st.info("Please select the dependent and independent variables to perform the analysis.")
 
         # Perform ANOVA analysis using statsmodels
         X = sm.add_constant(df[ind_variable])
